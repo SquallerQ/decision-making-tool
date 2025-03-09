bc4eb17385fdbf1f916cb631c8bbc35dcf54dab6
zSqualler <john199502031@gmail.com>
2025-03-09 22:59:36 +0100

    feat: install husky, change eslint and stylelint settings, change the folder structure

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import typescriptParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptPlugin,
+      unicorn: unicornPlugin,
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      ],
+      '@typescript-eslint/member-ordering': 'error',
+      'class-methods-use-this': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/number-literal-case': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            acc: true,
+            env: true,
+            i: true,
+            j: true,
+            props: true,
+            Props: true,
+          },
+        },
+      ],
+    },
+    ignores: ['node_modules/', 'dist/'],
+  },
+];
